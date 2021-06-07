063ac7ff075586027c8fd0a18b8d109d9c508035
miyashiiii <yosuke.miyashita@gmail.com>
2021-06-08 02:14:25 +0900

    feat: Add SE

--- Assets/NumButton.cs
+++ Assets/NumButton.cs
@@ -6,8 +6,13 @@ using UnityEngine.UI;
 
 public class NumButton : MonoBehaviour
 {
+    private AudioSource sound01;
 
+    void Start()
+    {
+        sound01 = GetComponent<AudioSource>();
 
+    }
     // Update is called once per frame
     void Update()
     {
@@ -16,6 +21,8 @@ public class NumButton : MonoBehaviour
 
     public void OnClick()
     {
+        sound01.PlayOneShot(sound01.clip);
+
         var text =GetComponentInChildren<Text>().text;
         int i = int.Parse(text);
         GameManager.OnButtonClick(i);
