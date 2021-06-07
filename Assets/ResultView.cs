9783346b3345f3249ba29c24f76862179669cb55
miyashiiii <yosuke.miyashita@gmail.com>
2021-06-08 00:54:57 +0900

    change: update ResultScene

---
+++ Assets/ResultView.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultView : MonoBehaviour
+{
+    private int time;
+    private int combo;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
