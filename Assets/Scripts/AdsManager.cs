9c777a44c21b236c62cabff7553e71738803fddf
miyashiiii <yosuke.miyashita@gmail.com>
2021-06-17 00:59:37 +0900

    feat: Add interstitial ads

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -6,30 +6,48 @@ using UnityEngine.Advertisements;
 public class AdsManager : MonoBehaviour
 {
     private const string GameIdIos = "4173276";
+
     private const string GameIdAndroid = "4173277";
-    private const string surfacingId = "Banner_Android";
+
+    // private const string BannerId = "Banner_Android";
+    private string _gameId;
+    private string _bannerId;
+    private static string _interstitialId;
     private const bool TestMode = true;
 
     void Start()
     {
 #if UNITY_ANDROID
-        Advertisement.Initialize(GameIdAndroid,TestMode);
+        _gameId = GameIdAndroid;
+        _interstitialId = "Interstitial_Android";
+        _bannerId = "Banner_Android";
 #elif UNITY_IOS
-            Advertisement.Initialize(GameIdIos);
+        _gameId = GameIdIos;
+        _interstitialId = "Interstitial_Ios";
+        _bannerId = "Banner_Ios";
 #else
+        return;
 #endif
-        
-    Advertisement.Banner.SetPosition (BannerPosition.BOTTOM_CENTER);
+
+        Advertisement.Initialize(_gameId, TestMode);
+
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
 
         StartCoroutine(ShowBannerWhenInitialized());
     }
 
-    IEnumerator ShowBannerWhenInitialized () {
-        while (!Advertisement.isInitialized) {
+    IEnumerator ShowBannerWhenInitialized()
+    {
+        while (!Advertisement.isInitialized)
+        {
             yield return new WaitForSeconds(0.5f);
         }
-        Advertisement.Banner.Show (surfacingId);
-    } 
- 
- 
+
+        Advertisement.Banner.Show(_bannerId);
+    }
+
+    public static void ShowInterstitial()
+    {
+        Advertisement.Show(_interstitialId);
+    }
 }
--- Assets/Scripts/RetryButton.cs
+++ Assets/Scripts/RetryButton.cs
@@ -2,8 +2,18 @@ using UnityEngine.SceneManagement;
 
 public class RetryButton : Button
 {
+    // 2回に1回広告を出す
+    private bool _adsToggle = false;
+
     protected override void OnClickInternal()
     {
+        if (_adsToggle)
+        {
+            AdsManager.ShowInterstitial();
+        }
+
+        _adsToggle = !_adsToggle;
+        
         SceneManager.LoadScene("GameScene");
     }
 }
